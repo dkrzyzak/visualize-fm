295e329deee25c483ed84118609faa71c34a65a9
apocalypseDreamer <damntheguitars@gmail.com>
2021-05-29 18:07:52 +0200

    fix url

--- src/axios.ts
+++ src/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const port = process.env.PORT || process.env.REACT_APP_API_PORT;
-const domain = process.env.REACT_APP_ON_HEROKU ? 'https://visualizefm.herokuapp.com' : 'http://localhost';
-const baseURL = `${domain}:${port}`;
-console.log(baseURL);
+const baseURL = process.env.REACT_APP_ON_HEROKU ? '' : `http://localhost:${port}`;
+
 export const instance = axios.create({
 	baseURL,
 });
